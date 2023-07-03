a9b3e188a79f12f3aa6ff8c37fc5ab13d8daf97c
Bourqu <dybourqu@gmail.com>
2023-07-03 19:09:06 -0400

    completed exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(input,count) {
+    if (count < 0){
+        return "ERROR"
 
+    }
+    
+    var result="";
+    for (let index = 0; index < count; index++) {
+        
+        result+=input;
+    }
+return result;
 };
+
+console.log(repeatString("hey",-1));
 
 // Do not edit below this line
 module.exports = repeatString;
