cb3e1f8bbc29487688bed0b8999cb838cbb4adba
Bourqu <dybourqu@gmail.com>
2023-07-03 19:28:07 -0400

    comp lesson 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let reverse =""
+    for (let index = input.length-1; index > -1; index--){
+        reverse+= input[index]
+        
+    }
+    return reverse;
 
 };
+
+reverseString("hello")
 
 // Do not edit below this line
 module.exports = reverseString;
