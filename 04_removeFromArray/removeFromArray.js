282b50922328835ef12cb98314dcff51dbec82ea
Bourqu <dybourqu@gmail.com>
2023-07-05 19:30:47 -0400

    completed challenge four using Rest paramter and the fileter function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(list,...values) {
+    
+    
+    for (const v  of values){
+        
+     const temp=list.filter(function (element){
+        return element !==v
+     })
+     
+     list=temp;   
+    }
+
+    console.log(list)
+
+    return list
+    
+
 
 };
 
+
+
+removeFromArray([1,2,3],2)
+removeFromArray([1,2,3],2,3)
 // Do not edit below this line
 module.exports = removeFromArray;
